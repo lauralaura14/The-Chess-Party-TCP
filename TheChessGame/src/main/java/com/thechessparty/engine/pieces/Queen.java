dc9489ebee8546394fd704d39c836f4c770bbc9f
Aharon Loiterman <54725988+aloiterman@users.noreply.github.com>
2021-04-25 16:06:04 -0400

    Implementation of Queen Class

--- TheChessGame/src/main/java/com/thechessparty/engine/pieces/Queen.java
+++ TheChessGame/src/main/java/com/thechessparty/engine/pieces/Queen.java
@@ -8,13 +8,74 @@ import java.util.List;
 
 public class Queen extends Piece{
 
+    //class variables
+    // offsets relative to the Queen's position on the board
+    private final static int[] QUEEN_MOVES = {-9,-8,-7,-1,1,7,8,9};
+
     public Queen(int position, final Team team){
         super(PieceIdentifiers.QUEEN, position,team);
     }
 
-    @Override
-    public List<Move> listLegalMoves(GameBoard board) {
-        return null;
+   @Override
+    public List<Move> listLegalMoves(final GameBoard board) {
+       
+        final List<Move> legalMoves = new ArrayList<>();
+       // return null;
+        for (final int current : QUEEN_MOVES) {
+            int destination = getPosition();
+              while (BoardUtilites.isValidMove(destination)) {
+                   if (isFirstColumn(destination, current) 
+                    || isEighthColumn(destination, current)) {
+                    break;
+                }
+                  destination += current;
+               
+                if (BoardUtilites.isValidMove(destination)) {
+                   final Tile destinationTile = board.getTile(current);
+
+                // if destination Tile is not occupied get NormalMove
+                if (!destinationTile.isTileOccupied()) {
+                    legalMoves.add(new NormalMove(board, this, destination));
+                } else {
+                    final Piece occupyingPiece = destinationTile.getPiece();
+                    final Team team = occupyingPiece.getTeam();
+
+                    // if the Tile is occupied get AttackMove
+                    if (getTeam() != team) {
+                        legalMoves.add(new AttackMove(board, this, destination, occupyingPiece));
+                    }
+                    break; 
+                }
+            }
+        }
+    }
+
+        return ImmutableList.copyOf(legalMoves);   
+}
+
+//----------- private helper methods ---------------------
+
+    /**
+     * Utilizes the constant boolean array that tracks the
+     *
+     * @param currentPosition current coordinates of the Queen
+     * @param offset          the offset position to be qualified
+     * @return true if the move can be made false if there is edge case exclusion
+     */
+    private static boolean isFirstColumn(final int currentPosition, final int offset) {
+        return BoardUtilites.FIRST_COLUMN[currentPosition] && (offset== -1 || offset == -9 || offset == 7);
+    }
+
+
+    /**
+     * Utilizes the constant boolean array that tracks the
+     *
+     * @param currentPosition current coordinates of the Queen
+     * @param offset          the offset position to be qualified
+     * @return true if the move can be made false if there is edge case exclusion
+     */
+    private static boolean isEighthColumn(final int currentPosition, final int offset) {
+        return BoardUtilites.EIGHTH_COLUMN[currentPosition] && (offset == -7 || offset == 1 || offset == 9);
     }
 
     /**
