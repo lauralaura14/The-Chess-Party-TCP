a6b8d98da57ab80a2b85a75e85826c5224d33bb4
Aharon Loiterman <54725988+aloiterman@users.noreply.github.com>
2021-04-27 02:44:41 -0400

    Implementation of King Class

--- TheChessGame/src/main/java/com/thechessparty/engine/pieces/King.java
+++ TheChessGame/src/main/java/com/thechessparty/engine/pieces/King.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class King extends Piece{
 
+    private final static int[] KING_MOVES = {-9, -8 , -7, -1, 1, 7, 8, 9};
+
     public King(int position, final Team team){
         super(PieceIdentifiers.KING, position, team);
     }
@@ -24,7 +26,66 @@ public class King extends Piece{
 
     @Override
     public List<Move> listLegalMoves(GameBoard board) {
-        return null;
+         
+         final List<Move> legalMoves = new ArrayList<>();
+
+         int destination;
+
+         for(final int current: KING_MOVES){
+
+            final int destination = getPiece() + current;
+
+            if(isFirstColumn(getPosition(), current) 
+                || isEighthColumn(getPosition(), current)){
+                continue;
+            }
+
+            if(BoardUtilites.isValidMove(destination)){
+                final Tile destinationTile = board.getTile(destination);
+
+                // if destination Tile is not occupied get NormalMove
+                if (!destinationTile.isTileOccupied()) {
+                    legalMoves.add(new NormalMove(board, this, destination));
+                } else {
+                    final Piece occupyingPiece = destinationTile.getPiece();
+                    final Team team = occupyingPiece.getTeam();
+
+                    // if the Tile is occupied get AttackMove
+                    if (getTeam() != team) {
+                        legalMoves.add(new AttackMove(board, this, destination, occupyingPiece));
+                    }
+                }
+            }
+
+         }
+
+
+
+         return ImmutableList.copyOf(legalMoves);
+    }
+
+       //----------- private helper methods ---------------------
+
+    /**
+     * Utilizes the constant boolean array that tracks the
+     *
+     * @param currentPosition current coordinates of the Knight
+     * @param offset          the offset position to be qualified
+     * @return true if the move can be made false if there is edge case exclusion
+     */
+    private static boolean isFirstColumn(final int currentPosition, final int offset) {
+        return BoardUtilites.FIRST_COLUMN[currentPosition] && ((offset == -9) || (offset == -1) || (offset == 7));
+    }
+
+        /**
+     * Utilizes the constant boolean array that tracks the
+     *
+     * @param currentPosition current coordinates of the Knight
+     * @param offset          the offset position to be qualified
+     * @return true if the move can be made false if there is edge case exclusion
+     */
+    private static boolean isEighthColumn(final int currentPosition, final int offset) {
+        return BoardUtilites.EIGHTH_COLUMN[currentPosition] && ((offset == -7) || (offset == 1)|| (offset == 9));
     }
 
     @Override
