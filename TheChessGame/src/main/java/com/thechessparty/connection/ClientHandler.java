9489ccde90b8281c94e4b58c684b152a888b3082
Laura <30821287+lauralaura14@users.noreply.github.com>
2021-05-02 13:25:30 -0400

    fixed disconnect problem for available user only. altered chat functionality to make all messages possible and not solely for game requests.

--- TheChessGame/src/main/java/com/thechessparty/connection/ClientHandler.java
+++ TheChessGame/src/main/java/com/thechessparty/connection/ClientHandler.java
@@ -45,12 +45,12 @@ public class ClientHandler implements Runnable {
 
             for (ClientHandler each : clientList) {
                 if (each.getStatus().equals("available"))
-                    each.outputStream.writeUTF("Current List: " + nameStatus.toString());
+                    each.outputStream.writeUTF("Current Available Waiting Player(s): " + nameStatus.toString());
             }
 
             outputStream.writeUTF("\nPlease message player you want to play with strictly following this format." +
-                    "\n'Request' to request game, 'no' to reject, 'yes' to accept. For example: " +
-                    "\nname: request | name: no | name: yes\n");
+                    "\n'game req' to make request, 'no req' to reject, 'yes req' to accept. For example: " +
+                    "\nname: game req | name: no req | name: yes req\n");
 
             //messaging client to client.
             outer:
@@ -64,7 +64,7 @@ public class ClientHandler implements Runnable {
 
                     for (ClientHandler each : clientList) {
                         if (each.getStatus().equals("available"))
-                            each.outputStream.writeUTF("Current List: " + nameStatus.toString());
+                            each.outputStream.writeUTF("Current Available Waiting Player(s): " + nameStatus.toString());
                     }
                     inputStream.close();
                     outputStream.close();
@@ -80,23 +80,23 @@ public class ClientHandler implements Runnable {
                 } else {
                     for (ClientHandler each : clientList) {  //see if name of person receiving msg exists
                         if (each.getClientName().toLowerCase().equals(receiverName.toLowerCase()) && !each.getStatus().equals("available")) {
-                            outputStream.writeUTF(each.clientName + " is unavailable. Make request to an available client.\n");
+                            outputStream.writeUTF(each.clientName + " is unavailable and cannot chat. Make request to an available client.\n");
                             break;
                         } else if (each.getClientName().toLowerCase().equals(receiverName.toLowerCase()) && each.getStatus().equals("available")) {
-                            if (!msg.equals("request") && !msg.equals("yes") && !msg.equals("no")) {
-                                outputStream.writeUTF("\nInvalid Response. Try again.\n");
-                                break;
-                            } else {
+                            //if (!msg.equals("request") && !msg.equals("yes") && !msg.equals("no")) {
+                            //    outputStream.writeUTF("\nInvalid Response. Try again.\n");
+                            //    break;
+                            //} else {
                                 each.outputStream.writeUTF(getClientName() + " says " + "'" + msg + "'");
-                            }
+                            //}
                             //person requesting automatically heads
                             switch (msg) {
-                                case "request":
+                                case "game req":
                                     break;
-                                case "no":
+                                case "no req":
                                     each.outputStream.writeUTF("\n" + getClientName() + " denied your request. Choose another available player.\n");
                                     break;
-                                case "yes":
+                                case "yes req":
                                     each.outputStream.writeUTF("\nSince you requested, you are automatically heads, " + each.clientName + ".\n...");
                                     outputStream.writeUTF("\nPlayer who accepts is automatically tails, " + this.clientName + ".\n...");
 
@@ -129,7 +129,7 @@ public class ClientHandler implements Runnable {
 
             for (ClientHandler each : clientList) {
                 if (each.getStatus().equals("available"))
-                    each.outputStream.writeUTF("Current List: " + nameStatus.toString());
+                    each.outputStream.writeUTF("Current Available Waiting Player(s): " + nameStatus.toString());
             }
 
         } catch (IOException ioException) {
